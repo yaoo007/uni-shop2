ad3ead1b875a6966e4ef60e2797c1a3bb7eed7f3
wangly <wly_1107@163.com>
2022-12-02 22:22:26 +0800

    init project

---
+++ main.js
@@ -0,0 +1,56 @@
+
+// #ifndef VUE3
+import Vue from 'vue'
+import App from './App'
+
+// 导入网络请求的包
+// 按需导入 $http 对象
+import { $http } from '@escook/request-miniprogram'
+
+// 在 uni-app 项目中，可以把 $http 挂载到 uni 顶级对象之上，方便全局调用
+uni.$http = $http
+// 请求的根路径
+$http.baseUrl = 'https://www.uinav.com'
+
+// 请求拦截器
+// 请求开始之前做一些事情
+$http.beforeRequest = function (options) {
+  // do somethimg...
+  uni.showLoading({
+    title:'数据加载中...'
+  })
+}
+
+// 响应拦截器 请求完成之后做一些事情
+$http.afterRequest = function () {
+  uni.hideLoading()
+}
+
+// 封装弹框的方法
+uni.$showMsg = function(title = '数据请求失败！', duration = 1500){
+  uni.showToast({
+    title,
+    duration,
+    icon:'none'
+  })
+}
+Vue.config.productionTip = false
+
+App.mpType = 'app'
+
+const app = new Vue({
+    ...App
+})
+app.$mount()
+// #endif
+
+// #ifdef VUE3
+import { createSSRApp } from 'vue'
+import App from './App.vue'
+export function createApp() {
+  const app = createSSRApp(App)
+  return {
+    app
+  }
+}
+// #endif
